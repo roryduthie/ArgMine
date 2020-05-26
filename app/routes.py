9851c1bb44b534a51cfe29f3d287ccf7d08b43db
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-05-26 18:36:36 +0100

    add calls from index to results

--- app/routes.py
+++ app/routes.py
@@ -17,11 +17,20 @@ def index():
 
 @app.route('/index', methods=['POST'])
 def index_post():
+    aif_mode = 'false'
+    han_mode = 'false'
+    external_text = request.form['edata']
+    source_text = request.form['sdata']
+    aif_mode = request.form['aif_mode']
+    session['s_text'] = source_text
+    session['e_text'] = external_text
+    session['aif'] = aif_mode
+    session['han'] = han_mode
     return redirect('/results')
 
 
 @app.route('/results')
 def render_text():
-
-    return render_template('results.html')
+    source_text = session.get('s_text', None)
+    return render_template('results.html', source_text=source_text)
 
