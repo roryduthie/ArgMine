0b6aef37cb270a24165c277de5742b1fa409feb0
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-05-26 18:06:07 +0100

    Adding website wireframes

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
+from flask import render_template, request, redirect, session, Markup
+from . import application
+import pandas as pd
+from urllib.request import urlopen
+import requests
+import json
+import urllib
+import tempfile
+import os
+import uuid
+
+
+@application.route('/')
+@application.route('/index')
+def index():
+    return redirect('/results')
+
+
+@application.route('/results')
+def render_text():
+
+    return render_template('results.html')
 
