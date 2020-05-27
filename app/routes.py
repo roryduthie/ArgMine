a1133d48c7bc4832b598973e12d5ce43f38ed66b
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-05-27 17:59:20 +0100

    added AMF calls

--- app/routes.py
+++ app/routes.py
@@ -49,7 +49,7 @@ def sent_to_df(txt):
     return df
 
 def predict_topic(df):
-    model_path = 'static/model/20192020topicmodel.joblib'
+    model_path = 'static/model/final_hansard_topic_model.joblib'
     with app.open_resource(model_path) as load_m:
         loaded_m = load(load_m)
     pred = loaded_m.predict(df['text'])
@@ -98,6 +98,145 @@ def get_hansard_text(file_path):
 
     with app.open_resource(file_path) as text_file:
         text = text_file.read()
-
+    text = text.encode('utf-8')
     print(text)
     return ''
+
+def text_to_lines(textData):
+    fin_list = []
+    lines_speakers = textData.splitlines(keepends=True)
+    for line in lines_speakers:
+        sentence_list = sent_tokenize(line)
+        if len(sentence_list) > 0 and len(sentence_list) < 2:
+            sent = sentence_list[0]
+            if len(sent) > 0:
+                fin_list.append(line)
+        elif len(sentence_list) > 0:
+            fin_list.append(line)
+
+    return fin_list
+
+def chunk_words(text_list):
+    word_counter = 0
+    chunks = []
+    temp_list = []
+    word_count_flag = False
+    for line in text_list:
+        words = line.split()
+        word_counter += len(words)
+        if word_counter > 700:
+            word_counter = len(words)
+            chunks.append(deepcopy(temp_list))
+            temp_list = []
+            word_count_flag = True
+        temp_list.append(line)
+    if word_counter < 700:
+        chunks.append(deepcopy(temp_list))
+    return chunks
+
+def aif_upload(url, aif_data):
+    aif_data = str(aif_data)
+    filename = uuid.uuid4().hex
+    filename = filename + '.json'
+    with open(filename,"w") as fo:
+        fo.write(aif_data)
+    files = {
+        'file': (filename, open(filename, 'rb')),
+    }
+    #get corpus ID
+
+    aif_response = requests.post(url, files=files, auth=('test', 'pass'))
+    #change this to pass the response back as text rather than as the full JSON output, this way we either pass back that a corpus was added to or a map uplaoded with map ID. Might be worth passing MAPID and Corpus name back in that situation.
+
+    os.remove(filename)
+    return aif_response.text
+
+def post_turns(url,text_str):
+    text_str = str(text_str)
+    filename = uuid.uuid4().hex
+    filename = filename + '.txt'
+    with open(filename,"w") as fo:
+        fo.write(text_str)
+    files = {
+        'file': (filename, open(filename, 'rb')),
+    }
+    #get corpus ID
+    response = requests.post(url, files=files)
+    os.remove(filename)
+    return response
+
+def post(url,text_str):
+    #print(text_str)
+    #text_str = str(text_str)
+    filename = uuid.uuid4().hex
+    filename = filename + '.txt'
+    with open(filename,"w") as fo:
+        fo.write(text_str)
+    files = {
+        'file': (filename, open(filename, 'rb')),
+    }
+    #get corpus ID
+
+    response = requests.post(url, files=files)
+    os.remove(filename)
+    return response
+
+def call_amf(chunks):
+    map_nums = []
+    url_turn = 'http://turninator.arg.tech/turninator'
+    url_props = 'http://propositionalizer.arg.tech/propositionalizer'
+    url_aif = 'http://www.aifdb.org/json/'
+    #URL for hosting outwith ARG-Tech Infrastrucutre
+    #url_inf = 'http://dam-02.arg.tech/dam-02'
+    url_inf = 'http://cicero.arg.tech:8092/dam-02'
+    for i, chunk in enumerate(chunks):
+        #print('######################################################')
+        #print('Processing chunk ' + str(i) + ' of ' + str(len(chunks)))
+        out_str = " ".join(chunk)
+        out_str = out_str.replace('’', '')
+        out_str = out_str.replace('‘', '')
+        out_str = out_str.replace(',', '')
+        out_str = out_str.replace('–', '')
+        out_str = out_str.replace(')', '')
+        out_str = out_str.replace('(', '')
+        out_str = out_str.replace("/", '')
+    #out_str = repr(out_str)
+        word_count = len(out_str.split())
+        #print(word_count)
+    #print(out_str)
+        #print('Getting Turns from AMF')
+        prop_text_resp = post_turns(url_turn, out_str)
+        prop_text = prop_text_resp.text
+    #print(prop_text)
+    #print(prop_text_resp)
+        if prop_text == '':
+        #print(prop_text)
+            print('EMPTY return TURNS')
+    #print(prop_text)
+        #print('Getting Propositions from AMF')
+        inf_text_resp = post(url_props, prop_text)
+        inf_text = inf_text_resp.text
+    #print(inf_text)
+        if inf_text == '':
+            print('EMPTY return PROPS')
+        #break
+        #print('Getting Inference relations from AMF')
+        aif_json_resp = post(url_inf, inf_text)
+        aif_json = aif_json_resp.text
+    #print(aif_json)
+        if aif_json == '':
+            print('EMPTY return INF')
+        #break
+        #print('Uploading AIF to AIFdb')
+    #print(aif_json)
+
+
+    #Commented out so as to not ruin AIFdb
+
+        #map_response = aif_upload(url_aif, aif_json)
+        #map_data = json.loads(map_response)
+        #map_id = map_data['nodeSetID']
+        #map_nums.append(map_id)
+        #print('Got nodeset ' + str(map_id) )
+    map_nums = [10672, 10670]
+    return map_nums
