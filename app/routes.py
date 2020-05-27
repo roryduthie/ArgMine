afaa5b4e1148ff59d9631009484bcf0a285108b3
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-05-27 16:44:46 +0100

    Add hansard file selection

--- app/routes.py
+++ app/routes.py
@@ -37,6 +37,9 @@ def render_text():
     print(source_text)
     txt_df = sent_to_df(source_text)
     result = predict_topic(txt_df)
+    hansard_fp = get_hansard_file_path('2020-05-24', result, 'HansardDataAMF')
+    print(hansard_fp)
+    get_hansard_text(hansard_fp)
     print(result)
     return render_template('results.html', source_text=source_text)
 
@@ -53,3 +56,48 @@ def predict_topic(df):
     result = pred[0]
     return result
 
+def get_hansard_file_path(input_date, topic, han_directory):
+    files_list = []
+    for subdir, dirs, files in os.walk(os.path.join(app.static_folder, han_directory)):
+        for file_name in files:
+            if 'txt' in file_name:
+                full_path = subdir + '/' + file_name
+                date = subdir.split(os.path.sep)[1]
+                date = date.replace("-","")
+                file = str(file_name).lower()
+                file_tup = (full_path, date, file)
+                files_list.append(file_tup)
+
+    sorted_files = sorted(files_list, key=lambda tup: tup[1], reverse=True)
+    print(sorted_files)
+    input_date = input_date.replace('-', '')
+    selected_file = ''
+    for tup in sorted_files:
+        date = tup[1]
+        file_name = tup[2]
+        file_path = tup[0]
+        if input_date < date:
+            continue
+        else:
+            if topic in file_name:
+                selected_file = file_path
+
+    if selected_file == '':
+        for tup in sorted_files:
+            date = tup[1]
+            file_name = tup[2]
+            file_path = tup[0]
+            if topic in file_name:
+                selected_file = file_path
+
+    if not selected_file == '':
+        selected_file = selected_file.split('/app/')[1]
+    return selected_file
+
+def get_hansard_text(file_path):
+
+    with app.open_resource(file_path) as text_file:
+        text = text_file.read()
+
+    print(text)
+    return ''
